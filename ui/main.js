c139e9078780feb626a3a615748b1e46f7508dd2
jagadeesh-24 <36101483+jagadeesh-24@users.noreply.github.com>
2018-02-20 20:56:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,15 +6,3 @@ button.onclick = function() {
     var shit=document.getElementById("count");
     shit.innerHTML=counter.toString();
     };
-
-var tester_button = document.getElementById("click_button");
-//tester_button.onclick = temp;
-
-var counter_value = 0;
-
-function temp () {
-    alert("on click function being fucked");
-    counter_value = counter_value + 1;
-    var display = document.getElementById("display_value");
-    display.innerHTML = String(counter_value);
-}
